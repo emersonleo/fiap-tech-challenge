acd5946940aac17c88ddbb13a80a320cf0df91e8
emersonleo <emerson.leonardo@ufrpe.br>
2025-05-22 22:57:13 -0300

    fix: remove caracteres especiais das mensagens

--- src/main/java/br/com/fiap/tech/challenge/grupo/vinte/oito/chefonline/dto/ProprietarioRequestDTO.java
+++ src/main/java/br/com/fiap/tech/challenge/grupo/vinte/oito/chefonline/dto/ProprietarioRequestDTO.java
@@ -12,7 +12,7 @@ public record ProprietarioRequestDTO(
         String nome,
 
         @NotBlank(message = "O campo 'email' deve ser preenchido")
-        @Email(message = "Digite um e-mail válido")
+        @Email(message = "Digite um e-mail valido")
         String email,
 
         @NotBlank(message = "O campo 'login' deve ser preenchido")
@@ -21,15 +21,15 @@ public record ProprietarioRequestDTO(
         @NotBlank(message = "O campo 'senha' deve ser preenchido")
         String senha,
 
-        @NotBlank(message = "O campo 'endereço' deve ser preenchido")
+        @NotBlank(message = "O campo 'endereco' deve ser preenchido")
         String endereco,
 
         @NotBlank(message = "O campo 'cnpj' deve ser preenchido")
-        @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter 14 dígitos numéricos")
+        @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter 14 dígitos numericos")
         String cnpj,
 
         @NotBlank(message = "O campo 'razão social' deve ser preenchido")
-        @Size(min = 5, max = 70, message = "A razão social deve ter entre 5 e 70 caracteres")
+        @Size(min = 5, max = 70, message = "A razao social deve ter entre 5 e 70 caracteres")
         String razaoSocial,
 
         @NotBlank(message = "O campo 'nome fantasia' deve ser preenchido")
